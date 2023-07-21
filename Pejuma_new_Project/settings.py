abc47ba0d882d59cd3012a24581cf199506ccd83
Godfred Opintan <godfredopintan77@gmail.com>
2023-07-21 19:57:51 +0000

    ...

--- Pejuma_new_Project/settings.py
+++ Pejuma_new_Project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pejuma_app.apps.PejumaAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
