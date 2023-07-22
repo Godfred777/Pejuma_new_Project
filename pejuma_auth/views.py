fa6eb661a6f41582886403f92f16cd83a8c96588
Godfred Opintan <godfredopintan77@gmail.com>
2023-07-22 11:37:48 +0000

    Authenticating new users is now implemented

--- pejuma_auth/views.py
+++ pejuma_auth/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def signup(request):
-    return render(request, 'pejuma_app/signup.html')
+    if request.method == "POST":
+        try:
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data['username']
+                messages.success(request, f'Account created for {username}')
+                return redirect('pejuma_app-categories')
+        except:
+            messages.error(request, f'Something went wrong. Please try again later')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'pejuma_app/signup.html', {'form':form})
