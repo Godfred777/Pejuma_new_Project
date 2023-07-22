d97f91b2097fa0c3fd9bb3fdd9bd19e86a3c1c5d
Godfred Opintan <godfredopintan77@gmail.com>
2023-07-22 02:19:57 +0000

    Create a Job and Review models for the database

--- pejuma_app/models.py
+++ pejuma_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=150)
+    discription = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    artisan = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
